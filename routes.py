c955b79a21da467c1929694dbb7347ef6595c333
ProjectOfAlmiraWool <24447@burnside.school.nz>
2024-07-21 17:54:04 +1200

    Add files via upload

--- routes.py
+++ routes.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/<name>") 
 def home(name):
-  return render_template("index.html", content=name)
+  return render_template("index.html", content=["alpaca", "cashmere", "sheep"])
 
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
