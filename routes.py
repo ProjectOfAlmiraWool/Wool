88c508e4d1c86c512d2f28ec31f2f4775eddb9a9
ProjectOfAlmiraWool <24447@burnside.school.nz>
2024-07-21 15:33:44 +1200

    Add files via upload

--- routes.py
+++ routes.py
@@ -1,18 +1,11 @@
-from flask import Flask, redirect, url_for 
+from flask import Flask, redirect, url_for, render_template 
 
 app = Flask(__name__)
 
 @app.route("/") 
 def home():
-  return "Welcome! this is wooly wishes <h1>WELCOME<h1>"
+  return render_template("index.html")
 
-@app.route("/<name>")
-def user(name):
-  return f"Welcome {name}!"
-
-@app.route("/admin")
-def admin():
-  return redirect(url_for("home")) 
 
 if __name__ == "__main__":
   app.run()
