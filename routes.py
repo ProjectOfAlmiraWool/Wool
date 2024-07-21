080f0e92eac6cc1c39cf6c3ab87fd67db87e14db
ProjectOfAlmiraWool <24447@burnside.school.nz>
2024-07-21 22:10:59 +1200

    Add files via upload

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
-from flask import Flask, redirect, url_for, render_template 
+from flask import Flask, redirect, url_for, render_template, request, session  
 
 app = Flask(__name__)
+app.secret_key = "hello"
 
 @app.route("/<name>") 
 def home(name):
@@ -8,11 +9,28 @@ def home(name):
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
-  return render_template()
-
-@app.route("/<usr>")
-def user(usr):
-  return f"<h1>{usr}</h1>"
+  if request.method == "POST": 
+      user = request.form["nm"]
+      session["user"] = user 
+      return redirect(url_for("user"))
+  else:  
+      if "user" in session:
+        return redirect(url_for("user"))    
+      
+      return render_template("login.html")
+    
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
    
+@app.route("/logout")
+def logout():
+  session.pop("user", None)
+  return redirect(url_for("login")) 
+
 if __name__ == "__main__":
   app.run(debug=True)
