1e73114a099a18a800706ea817fe15f875ff1631
ProjectOfAlmiraWool <24447@burnside.school.nz>
2024-07-29 09:39:03 +1200

    Add files via upload

--- routes.py
+++ routes.py
@@ -30,6 +30,15 @@ def login():
     session.permanent = True 
     user = request.form["nm"]
     session["user"] = user 
+    found_user = users.query.filter_by(name=user).first()  
+    if found_user:
+      session["email"] = found_user.email  
+    else:
+      usr = users(user, "")
+      db.session.add(usr)
+      db.commit()
+
+
     flash("Login Successful!")
     return redirect(url_for("user"))
   else:  
