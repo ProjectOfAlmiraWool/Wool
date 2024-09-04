5ca576616dd2e854b11ab8e3baf20060e8497ae3
ProjectOfAlmiraWool <24447@burnside.school.nz>
2024-09-04 22:43:48 +1200

    Add files via upload

--- routes.py
+++ routes.py
@@ -1,23 +1,48 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash  
 from datetime import timedelta
-
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 app.secret_key = "hello"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(days=1)
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+  _id = db.Column("id", db.Integer, primary_key=True)
+  name = db.Column(db.String(100))
+  email = db.Column(db.String(100))  
+  
+  def __init__(self, name, email):
+    self.name = name
+    self.email = email 
 
 
 @app.route("/<name>")
 def home(name):
     return render_template("index.html")
 
-
+@app.route("/view")
+def view():
+  return render_template("view.html", values=users.query.all())
+  
 @app.route("/login", methods=["POST", "GET"])
 def login():
   if request.method == "POST":
     session.permanent = True
     user = request.form["nm"]
     session["user"] = user
+    
+    found_user = users.query.filter_by(name=user).first() 
+    if found_user:
+      session["email"] = found_user.email
+    else:
+      usr = users(user, "")
+      db.session.add(usr)
+      db.session.commit()
+    
     flash("Login Successful!")
     return redirect(url_for("user"))
   else:  
@@ -36,6 +61,9 @@ def user():
         if request.method == "POST":
           email = request.form["email"]
           session["email"] = email
+          found_user = users.query.filter_by(name=user).first() 
+          found_user.email = email
+          db.session.commit()
           flash("Email was saved!")
         else:
           if "email" in session:
@@ -51,12 +79,13 @@ def logout():
   if "user" in session:
         user = session["user"]
         flash("You have been logged out!", "info")
-  session.pop("user", None)
-  session.pop("email", None)
+        session.pop("user", None)
+        session.pop("email", None)
   return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
+  app.app_context()
   app.run(debug=True)
 
 @app.route("/user")
