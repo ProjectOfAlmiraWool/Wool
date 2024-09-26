45820be4e266698895cb3df5bd616b83d9e72357
ProjectOfAlmiraWool <24447@burnside.school.nz>
2024-09-26 23:07:28 +1200

    Add files via upload

--- routes.py
+++ routes.py
@@ -1,76 +1,33 @@
-from flask import Flask, redirect, url_for, render_template, request, session, flash
-from datetime import timedelta
-
+from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
-app.secret_key = "hello"
-app.permanent_session_lifetime = timedelta(days=1)
 
-
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
- 
-@app.route("/login", methods=["POST", "GET"])
-def login():
-  if request.method == "POST":
-    session.permanent = True
-    user = request.form["nm"]
-    session["user"] = user  
-   
-    flash("Login Successful!")
-    return redirect(url_for("user"))
-  else:  
-    if "user" in session:
-        flash("Already Logged In!")
-        return redirect(url_for("user"))    
-     
-    return render_template("login.html")
-   
-@app.route("/user", methods=["POST", "GET"])
-def user():
-    email = None
-    if "user" in session:
-        user = session["user"]
-       
-        if request.method == "POST":
-          email = request.form["email"]
-          session["email"] = email
-          flash("Email was saved!")
-        else:
-          if "email" in session:
-              email = session["email"]
-         
-        return render_template("user.html", email=email)
-    else:
-        flash("You are not logged in!")
-        return redirect(url_for("login"))
-   
-@app.route("/logout")
-def logout():
-  if "user" in session:
-        flash("You have been logged out!", "info")
-        session.pop("user", None)
-        session.pop("email", None)
-  return redirect(url_for("login"))
+    return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
+@app.route('/all_wools')
+def all_wools():
+    conn = sqlite3.connect('wool.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Wool")
+    wools = cur.fetchall()
+    conn.close()
+    return render_template('all_wools.html', wools=wools)
+  
+@app.route('/sizes')
+def sizes():
+    conn = sqlite3.connect('woold.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Wool")
+    sizes = cur.fetchall()
+    conn.close()
+    return render_template('sizes.html', sizes=sizes) 
 
-
-if __name__ == "__main__":
-  app.app_context()
-  app.run(debug=True)
-
-
-@app.route("/user")
-def user():
-    if "user" in session:
-        user = session["user"]
-        return render_template("user.html", user=user)
-    else:
-        flash("You are not logged in!")
-        return redirect(url_for("login"))
-
-
-if __name__ == "__main__":
-  app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
